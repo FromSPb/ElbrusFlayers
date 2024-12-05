790e040a918f7237970b4e53321192f544f806cf
FromSPb <www.14088@gmail.com>
2024-12-05 13:07:30 +0300

    andrei yvolen

--- client/src/Widgets/Animals/AnimalCard.jsx
+++ client/src/Widgets/Animals/AnimalCard.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 //name, type, description
 function AnimalCard({animal}) {
+
+    const [faf , setFaf] = useState(false)
+
+    const click = async () => {
+        setFaf((prev) => !prev)
+
+    }
+
     return (
         <>
-            
+
             {animal.name}
             {animal.type}
+            <button onClick={click}>изменить</button>
+            {faf===true &&(
+                <h1>1</h1>
+            )}
         </>
     );
 }
