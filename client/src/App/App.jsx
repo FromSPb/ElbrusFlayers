1d3fe9543081d457d3805046d57f9d1d92f87e88
FromSPb <www.14088@gmail.com>
2024-12-06 13:01:10 +0300

    pre final logic

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,24 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Layout from "../Widgets/Layout/Layout";
+
 import StartPage from "../Pages/StartPage/StartPage";
 import PricePage from "../Pages/PricePage/PricePage";
 import SignInPage from "../Pages/SignInPage/SignInPage.jsx";
 import SignUpPage from "../Pages/SignUpPage/SignUpPage.jsx";
 import ImagePage from "../Pages/ImagePage/ImagePage";
 import AnimalsPage from "../Pages/AnimalsPage/AnimalsPage";
-
 import { useEffect, useState } from "react";
 import { setAccessToken } from "../Shared/lib/axiosInstance.js"
-import Navigation from "../Widgets/Navigate/Navigation.jsx";
-// import { Layout } from "antd";
 import UserApi from "../Entites/Users/UserApi.js";
+import Layout from "../Widgets/Layout/Layout.jsx";
 
 function App() {
   const [user, setUser] = useState(null);
-
-  // console.log(user);
-
-  //NOTE - постоянный перезапрос данных по юзеру и токену
   useEffect(() => {
     UserApi.refreshTokens()
       .then(({ error, data, statusCode }) => {
@@ -36,23 +30,38 @@ function App() {
       });
   }, []);
 
-  //NOTE - юзера можно прокидывать вниз по роутингу
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Navigation user={user} setUser={setUser} />,
+      element: <Layout user={user} setUser={setUser} />,
       children: [
-        { path: "/", element: <StartPage /> },
-        { path: "/animals", element: <AnimalsPage user={user} setUser={setUser} /> },
-        { path: "/price", element: <PricePage setUser={setUser} /> },
-        { path: "/signIn", element: <SignInPage setUser={setUser} /> },
-        { path: "/signUp", element: <SignUpPage setUser={setUser} /> },
-        { path: "/image", element: <ImagePage setUser={setUser} /> },
-
+        { 
+          path: "/",
+          element: <StartPage /> 
+        },
+        { 
+          path: "/animals", 
+          element: <AnimalsPage user={user} setUser={setUser} /> 
+        },
+        {
+          path: "/price", 
+          element: <PricePage setUser={setUser} /> 
+        },
+        { 
+          path: "/signIn", 
+          element: <SignInPage setUser={setUser} /> 
+        },
+        { 
+          path: "/signUp", 
+          element: <SignUpPage setUser={setUser} /> 
+        },
+        { 
+          path: "/image", 
+          element: <ImagePage setUser={setUser} /> 
+        },
       ],
     },
   ]);
-
   return <RouterProvider router={router} />;
 }
 
